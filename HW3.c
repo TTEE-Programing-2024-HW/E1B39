fee99e9b6e58df10a9ac9c8d617ce7a1e01952dd
KUOMING-HAN <u11202239@o365.ttu.edu.tw>
2024-05-18 23:37:02 +0800

    第一版 加入個人化面但是還沒改完

---
+++ HW3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int num1;
+	char c;
+	
+	printf("***********************************************************\n");
+	printf("***********************************************************\n");
+	printf("*                                                         *\n");
+	printf("*                    E1B39   ³¢©ú¿«                       *\n");
+	printf("*                                                         *\n");
+	printf("*                                                         *\n");
+	printf("*                                                       *\n");
+	printf("*                                                   *\n");
+	printf("*                                                   *\n");
+	printf("*                       *** **                            *\n");
+	printf("*                          *                              *\n");
+	printf("*                          *                              *\n");
+	printf("*                          *                              *\n");
+	printf("*                          *                              *\n");
+	printf("*                        *   *                            *\n");
+	printf("*                       *     *                           *\n");
+	printf("*                       *     *                           *\n");
+	printf("*                     ***     ***                         *\n");
+	printf("***********************************************************\n");
+	printf("***********************************************************\n");
+	system("pause");
+	system("CLS");
+
+	printf("½Ð¿é¤J±K½X:");
+	scanf("%d",&num1);
+	if(num1==2024)
+	{
+		printf("\nWelcome");
+	} 
+	else
+	{
+		printf("Error!\a");
+		return 0;
