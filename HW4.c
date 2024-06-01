1d7506c9bd67229dbf04d3285c0ead1dee3b4472
KUOMING-HAN <u11202239@o365.ttu.edu.tw>
2024-06-01 22:57:38 +0800

    第二版 加入選單跟輸入學生狀態的程式的程式

--- HW4.c
+++ HW4.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int main(void)
 {
 	int num1,a=3;
@@ -45,3 +46,88 @@ int main(void)
 	
     		return 0;
 }	 
+    printf("|--------------------[Grade  System]----------------------|\n");
+	printf("|       'a' :  Enter student grades                       |\n");
+	printf("|---------------------------------------------------------|\n");
+	printf("|       'b' :  Display student grades                     |\n");
+	printf("|---------------------------------------------------------|\n");
+	printf("|       'c' :  Search for student grades                  |\n");
+	printf("|---------------------------------------------------------|\n");
+	printf("|       'd' :  Grade ranking                              |\n");
+	printf("|---------------------------------------------------------|\n");
+	printf("|       'e' :  Exit system                                |\n");
+	printf("-----------------------------------------------------------\n"); 
+char choice;
+    printf("請輸入選項:");
+    scanf("%d",&choice);
+    getchar();
+    switch (choice)
+	{
+    	case 'a':
+    		
+    		
+    	case 'b':	
+    		
+    		
+    	case 'c':
+    	
+		
+		case 'd':
+		
+		
+		
+		case 'e':
+		
+		
+		
+		
+		
+	{
+		int n,i;
+		
+		printf("輸入學生的數量(5~10):\n");
+		scanf("%d",&n);
+		if(n<5||n>10)
+		{
+			printf("請重新輸入");
+			return;
+		}
+		for(i=0;i<n;i++)
+		{
+			printf("請輸入學生的姓名:");
+			scanf("%s",students[studentcount].name);
+			printf("請輸入學號:");
+			scanf("%d",&students[studentcount].id);
+			printf("請輸入數學成績:");
+			scanf("%d",&students[studentcount].math);
+			printf("請輸入物理成績:");
+			scanf("%d",&students[studentcount].physics);
+			printf("請輸入英文成績:");
+			scanf("%d",&students[studentcount].english);
+			
+			
+		}
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
